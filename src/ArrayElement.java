aaeac6a234a623796ec19e7df64692e8e96d9010
devikakarpe <devikankarpe@gmail.com>
2023-03-08 21:42:21 +0530

    added logic for adding array element

---
+++ src/ArrayElement.java
@@ -0,0 +1,12 @@
+public class ArrayElement {
+    public static void main(String[] args) {
+
+        //Initialize Array
+        int [] arr = new int [] {1,2,3,4,5,6,7,8,9,10};
+        System.out.println("Elements of array are: ");
+
+        //Using for loop to increament the value of i
+        for(int i=0;i<arr.length;i++)
+            System.out.print(arr[i] + " ");
+    }
+}
