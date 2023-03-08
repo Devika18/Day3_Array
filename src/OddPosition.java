15361c537323a9481592c2189af5e4c4519de8bf
devikakarpe <devikankarpe@gmail.com>
2023-03-08 21:59:26 +0530

    added logic for finding odd position element

---
+++ src/OddPosition.java
@@ -0,0 +1,12 @@
+public class OddPosition {
+    public static void main(String[] args) {
+        int [] a = new int [] {10,20,30,40,50};
+        System.out.println("Elements of array present on odd position are: ");
+
+        //Initializing for loop through the array by increamenting value of i by 2
+        for(int i=0;i<a.length;i=i+2)
+        {
+            System.out.println(a[i]);
+        }
+    }
+}
